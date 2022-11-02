881d1a10ec35224f36e4a80020eddcf5af4acef2
Ben Halpern <bhalpern@veracode.com>
2022-11-02 16:17:37 -0700

    added some comments and checking os commands to the script

--- Scripts/Dev/Script1.sh
+++ Scripts/Dev/Script1.sh
@@ -1,21 +1,23 @@
-#! /bin/bash
-# Author:       Ben-Ami Halpern - Veracode 
+# Author:       Ben-Ami Halpern - Veracode
 # Contatct:     bhalpern@veracode.com
-# Purpose:      
+# Purpose:
 # Date:         9/6/22
 # In order to use this script run it on a linux machine and you must have docker installed and created a credential file as specified by Veracode Documentation
-purpose=" This is the purpose of this script is to make utilizing veracodes APIs easier"
-
-#Script Start
-echo ============================================================
-echo ==================== DEVELOPMENT SCRIPT ====================
-echo ============================================================
-echo Description:
-echo $purpose
-echo 
-docker --help
+echo "============================================================"
+echo "==================== DEVELOPMENT SCRIPT ===================="
+echo "============================================================"
+echo "Description: The purpose of this script is to utilize veracodes APIs easier"
+echo "+-----------------------------------------------------------+"
+echo "+--------- Checking to see if Docker is installed ----------+"
+echo "+-----------------------------------------------------------+"
+echo "if this script is running in a wsl2 please start docker \n before running this script"
+if [!docker --version ]; then
+echo "docker is not installed"
+else
+echo "docker is operational"
+fi
 #TODO: check to see that you have docker installed
 #TODO: check to see that you have the API credentials in the correct folder
-
 #TODO: Go down the API documentation and get a list of all the commands and create a template style for a menu of commands
 #TODO: Show Menu
+#EOF
--- Scripts/Dev/script2.py
+++ Scripts/Dev/script2.py
@@ -1,10 +1,14 @@
-#! python3
+#! /env/python3
 # File:     
 # Author:   Ben Halpern - Veracode
 # Role:     Associate CSE
 # Manager:  Justin Yao
 # Date:     09/06/22
-# 
+#
+import subprocess
+import platform                                                # Used to determine the operating system
+import os                                                      # Used to determine the operating system and for running shell commands
+ 
 """DOCSCRIPT:
 The purpose of this script is to provide a convenient script that runs checks and installs 
 the various veracode SaaS on the computer. It provides walk throughs to specify what is needed next and where to get it
@@ -14,7 +18,7 @@ Hooks into APIs to provide a UI for the API backend
 
 """
 
-
+# This is an MD5SUM of the Pipeline Scanner Zip
 SAST_MD5SUM = "92a99cfa495d948f6ad72afd9668a939"
 print("==============================================================")
 print("================  Veracode SAST Installer ====================")
@@ -27,22 +31,76 @@ print("==============================================================")
 #           httpie method
 #           actions menu
 
+#subprocess.run()
+################################################################################################################################################################################################################################
+# Determining the enviornment ##################################################################################################################################################################################################
+################################################################################################################################################################################################################################
 
-print("In order to use this script, you need to have the proper veracode permissions")
+
+# Check to see the operating system
+print("\n\n==============  Determining operating system =================\n\n")
+print("Name of the operating system: ", os.name)
+print("Name of the OS System is running on: ", platform.system())
+print("Name of the Operating System version: ", platform.release())
+print("Name of the platform Machine: ", platform.machine())
+print("Name of the platform architecture: ", platform.architecture())
+
+osname = os.name
+osplatform = platform.system()
+osversion = platform.release()
+osprocess = platform.machine()
+osarch = platform.architecture()
+
+
+################################################################################################################################################################################################################################
+# Enviornment Conditional Logic ################################################################################################################################################################################################
+################################################################################################################################################################################################################################
+
+
+
+# Pass this information to a condinal logic or switch and then run the commands accordingly
+# Depending on the enviornmnet, determine if is being run in a CI/CD
+# Depending on the enviornment, if not being run in a CI/CD then find out the user name in order to find the credentials file
+    # otherwise prompt the user for the location of the credentials file
+    # otherwise prompt the user for the creation of a credentials file
+        # Request the user to go to the platform and generate API keys, and provide them into the script, the script will initialize the credentials file
+
+if(osname == "nt" or osplatform == "Windows"):
+    print("You are running a windows based machine")
+    # Windows command block
+
+elif(osname == "posix" or osplatform == "Linux"):
+    print("You are running on a linux based machine")
+    # linux command block
+
+else:# Todo: add a mac os block
+    print("Your machine is currently undeterminable")
+
+
+
+
+################################################################################################################################################################################################################################
+# API Credentials  #############################################################################################################################################################################################################
+################################################################################################################################################################################################################################
+
+
+print("\n\nIn order to use this script, you need to have the proper veracode permissions")
 #TODO: check prerequesites:
 #       - Check OS
 #       - Check version of python installed
 #       - check to see logged in username and location to save api credentials file
 
+# Check to see if credentials file exists otherwise prompt the user to generate credentials
 
 
-print("You must aquire api credentials and place them in a file as specified by the veracode documentation")
-print("1) Log in to the Veracode Platform.")
-print("2) From the user account dropdown menu, select API Credentials option. Alternatively, from the homescreen click on the Generate API Credentials button if available")
-print("Click Generate API Credentials")
-print("Copy the ID and secret key to a secure place. Veracode recommends storing your credentials in an API Credentials file")
-api_id_input = input("API ID: ")
-api_key_input = input("API_KEY: ")
+
+#print("You must aquire api credentials and place them in a file as specified by the veracode documentation")
+#print("1) Log in to the Veracode Platform.")
+#print("2) From the user account dropdown menu, select API Credentials option. Alternatively, from the homescreen click on the Generate API Credentials button if available")
+#print("Click Generate API Credentials")
+#print("Copy the ID and secret key to a secure place. Veracode recommends storing your credentials in an API Credentials file")
+#api_id_input = input("API ID: ")
+#api_key_input = input("API_KEY: ")
 #TODO: check to see if the credentials file exists
 #TODO: if exists move the old one to credentials.old
 #TODO: create new credential file with the format
@@ -50,7 +108,7 @@ api_key_input = input("API_KEY: ")
 # [default]
 # veracode_api_key_id = {your_api_key_id}
 # veracode_api_key_secret = {your_api_secret_key}
-print("-----------  Creating API Credentials file  -------------")
+#print("-----------  Creating API Credentials file  -------------")
 #TODO: Create a file with python and load the credentials in the file.
 #TODO: verify that the credential file was created succesfully
 
@@ -118,7 +176,7 @@ print("-----------  Creating API Credentials file  -------------")
 
 
 
-print("Testing out api credentials")
+#print("Testing out api credentials")
 #TODO: Add link
 #TODO: API Credentials initialization
 #TODO: Check that docker is installed
