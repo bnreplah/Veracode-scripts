1b43c1554ffa64ae168043bd3fc0dd0249904c21
Ben H <46547251+bnreplah@users.noreply.github.com>
2024-06-27 10:00:27 -0700

    Update DAST-rescan.sh
    Signed-off-by: Ben H <46547251+bnreplah@users.noreply.github.com>

--- Scripts/Release/DAST-rescan.sh
+++ Scripts/Release/DAST-rescan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Usage: DAST-rescan.sh [Analysis Name <String>] [Duration <INT>] [Start Now <Bool>] [Start Date String <UTC Date>]
 analysis_name="$1"
 duration="$2"
 
@@ -8,9 +9,10 @@ today=$(date +"%Y-%m-%dT23:00+00:00") # always at 11:00
 #iso_date=$(date -I)
 date=""
 
-# if [[ "$3" -eq "true" ]]; then
-#   now=true
-# fi
+#Comment this out to have it use the date parameter for the start time instead only
+if [[ "$3" -eq "true" ]]; then
+   now=true
+fi
 
 if [[ -z "$4" ]]; then
   date="$today"
@@ -20,12 +22,12 @@ else
 fi
 
 http --auth-type veracode_hmac "https://api.veracode.com/was/configservice/v1/analyses?name=$1" -o analysis.json
-
+# if there are multiple names matching the criteria will select the first one
 name=$(cat analysis.json | jq -r '._embedded.analyses[0].name')
 analysis_id=$(cat analysis.json | jq -r '._embedded.analyses[0].analysis_id')
 lt_occurrence_id=$(cat analysis.json | jq -r '._embedded.analyses[0].latest_occurrence_id')
 lt_occurrence_status=$(cat analysis.json | jq -r '._embedded.analyses[0].latest_occurrence_status.status_type')
-
+# TODO: Error handling
 if [ -z "$name" ]; then
   echo "Analysis Name not found"
   exit
@@ -37,6 +39,7 @@ echo """
 {
   \"name\": \"$name\",
   \"schedule\": {
+   \"now\": \"$now\",
     \"start_date\": \"$date\",
     \"duration\": {
       \"length\": $duration,
@@ -44,6 +47,20 @@ echo """
         }
     }
 }""" > input.json
+
+# Use this echo statement to make the script trigger scans immidiately
+#echo """
+#{
+#  \"name\": \"$name\",
+#  \"schedule\": {
+#   \"now\": \"true\",
+#    
+#    \"duration\": {
+#      \"length\": $duration,
+#      \"unit\": \"DAY\"
+#        }
+#    }
+#}""" > input.json
 
 echo "Sending the following payload: "
 cat input.json
